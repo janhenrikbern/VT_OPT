a66b67eaa1354ae77a8c196047f0beac6623f261
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2020-11-02 21:11:24 -0500

    Adding base simulation

--- car.py
+++ car.py
@@ -1,7 +1,9 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import image
-from math import sin, cos
+from math import (
+    sin, 
+    cos, 
+    sqrt,
+)
 
 class PointCar:
     def __init__(self, *starting_coordinates):
@@ -20,24 +22,32 @@ class PointCar:
         """
         # control inputs
         self.a = 0.0
-        self.steering_angle = 0.0
-
-        # constant car attributes
-        self.weight = 1.0
-
+        self.steering_angle = 0.0 
 
         self.location = list(starting_coordinates)
 
         self.dt = 0.1 #sec
 
-    def step(self):
-        v = self.a * self.dt
-        dx = self.v * cos(self.steering_angle)
-        dy = self.v * sin(self.steering_angle)
+    def update(self, acceleration, steering_input):
+        self.a = acceleration
+        self.steering_angle = steering_input
 
-        self.location[0] += dx
-        self.location[1] += dy
+    def step(self):
+        self.v = self.a * self.dt
+        self.dx = self.v * cos(self.steering_angle)
+        self.dy = self.v * sin(self.steering_angle)
+
+        self.location[0] += self.dx
+        self.location[1] += self.dy
 
     def get_location_and_heading(self):
-        return self.location[0], self.location[1], self.steering_angle
+        magnitude = sqrt(self.dx**2 + self.dy**2)
+        return self.location[0], self.location[1], self.dx / magnitude, self.dy / magnitude
+
+    def plot_state(self):
+        x, y, dx, dy = self.get_location_and_heading()
+        plt.arrow(x, y, 5*dx, 5*dy, width=1.0)
+    
+    def __str__(self):
+        return f"PointCar - Location (x,y): {self.location}, acceleration: {self.a}"
 
--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import image
 
-IMG_PATH = "./tracks/track.png"
+from track import load_track
+from car import PointCar
+import random
 
-def load_track(img_path):
-	img = image.imread(img_path)
-	img_array = np.asarray(img)
-	track_matrix = transform_to_track(img_array)
-	return track_matrix
-
-def transform_to_track(img):
-	matrix = np.sum(img, axis=2)
-	normalized = matrix / np.linalg.norm(matrix)
-	return (normalized / np.max(normalized)) < 0.5
-
-track = load_track(IMG_PATH)
-plt.imshow(track)
-plt.show()
 
 
 def find_valid_trajectory(track):
 	pass
 
-
 def optimize_trajectory(track, trajectory):
 	pass
 
+
+if __name__ == "__main__":
+	IMG_PATH = "./tracks/track.png"
+	track = load_track(IMG_PATH)
+
+	# Set to a valid point in trajectory
+	car = PointCar(50, 100)
+	fig = plt.figure()
+	
+	for i in range(500):
+		print(f"Step {i+1}")
+		car.update(5.0, 1.0 + (random.random() - 0.5))
+		car.step()
+		# print(car)
+
+		if i % 100 == 0:
+			plt.imshow(track)
+			car.plot_state()
+			plt.show()
