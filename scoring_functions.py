ba3a680973bd19645659a7afcaf7378e899bce8c
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2020-11-29 16:51:11 -0500

    unify scoring functions

--- scoring_functions.py
+++ scoring_functions.py
@@ -5,14 +5,12 @@ def distance_score(node, x, y):
         return None
     dx, dy = node.vehicle.get_distance_components((x, y))
     dist = sqrt(dx**2 + dy**2)
-    print(f"distance to next location: {dist}")
-    score = node.val + 1 / (dist + 1)
+    score = node.val + 1.0 / (dist + 1.0)
     return score
 
 def time_score(node, x, y):
     if node.vehicle.can_reach_location((x,y)) is False:
         return None
     dt = node.vehicle.get_time((x,y))
-    print(dt)
-    score = node.val + 1 / log(1.0 + dt)
+    score = node.val + 1 / (dt + 1.0)
     return score
