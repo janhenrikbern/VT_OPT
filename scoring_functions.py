a917fc1539fb69dd460cd232ea62ca9e6cc2ab8d
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2020-11-30 10:06:03 -0500

    create separate scoring function for baseline

--- scoring_functions.py
+++ scoring_functions.py
@@ -24,3 +24,9 @@ def time_score(node, x, y):
     dt = node.vehicle.get_time((x,y))
     score = node.val + 1 / (dt + 1.0)
     return score
+
+def centerline_score(node, x, y):
+    dx, dy = node.vehicle.get_distance_components((x, y))
+    dist = sqrt(dx**2 + dy**2)
+    score = node.val + 1.0 / (dist + 1.0)
+    return score
